f081970c17856b79f033f1db50bbfde91ff5b440
Rich Lott <forums@artfulrobot.uk>
2016-03-11 09:52:35 +0000

    Do settings the way given in the docs instead of my bodge

--- CRM/Loglop/Form/LogLopSettings.php
+++ CRM/Loglop/Form/LogLopSettings.php
@@ -7,53 +7,106 @@ require_once 'CRM/Core/Form.php';
  *
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
  */
-class CRM_Loglop_Form_LogLopSettings extends CRM_Admin_Form_Setting {
 
-  // This is field => group, but I'm not sure what group is.
-  // Having this seems to mean the value is saved into civicrm_setting table.
-  protected $_settings = ['loglop_job_log_age' => 'loglop_job_log_age'];
+class CRM_Loglop_Form_LogLopSettings extends CRM_Core_Form {
+  private $_settingFilter = array('group' => 'loglop');
+  // The code following is taken from the example file at
+  // <https://github.com/eileenmcnaughton/nz.co.fuzion.civixero/blob/master/CRM/Civixero/Form/XeroSettings.php>
+  // except the getFormSettings() function which in the example has some custom
+  // stuff we don't need here.
+  private $_submittedValues = array();
+  private $_settings = array();
+  function buildQuickForm() {
+    $settings = $this->getFormSettings();
+    foreach ($settings as $name => $setting) {
+      if (isset($setting['quick_form_type'])) {
+        $add = 'add' . $setting['quick_form_type'];
+        if ($add == 'addElement') {
+          $this->$add($setting['html_type'], $name, ts($setting['title']), CRM_Utils_Array::value('html_attributes', $setting, array ()));
+          //function HTML_QuickForm_select($elementName=null, $elementLabel=null, $options=null, $attributes=null)
+        }
+        else {
+          $this->$add($name, ts($setting['title']));
+        }
+        $this->assign("{$setting['description']}_description", ts('description'));
+      }
+    }
 
-  public function buildQuickForm() {
-
-    $result = civicrm_api3('Setting', 'get', array(
-      'sequential' => 1,
-      'return' => "loglop_job_log_age",
-    ));
-    // Q. how to set the value in the form?
-    // A. this seems to do it...
-    $this->_defaultValues['loglop_job_log_age'] = isset($result['values'][0]['loglop_job_log_age']) ? $result['values'][0]['loglop_job_log_age'] : null;
-    // add form elements
-    $this->add(
-      'select', // field type
-      'loglop_job_log_age', // field name
-      'Delete logs older than', // field label
-      array(
-        ''         => ts('Do not delete any logs'),
-        '2 years'  => ts('2 Years'),
-        '1 year'   => ts('1 Year'),
-        '6 months' => ts('6 Months'),
-        '1 month'  => ts('1 Month'),
-        '1 day'    => ts('1 Day'),
-      ),
-      FALSE // is required - defaults to doing nothing.
-    );
     $this->addButtons(array(
-      array(
+      array (
         'type' => 'submit',
         'name' => ts('Submit'),
         'isDefault' => TRUE,
-      ),
+      )
     ));
 
     // export form elements
-    $this->assign('elementNames', ['loglop_job_log_age']);
+    $this->assign('elementNames', $this->getRenderableElementNames());
+    parent::buildQuickForm();
   }
-
-  public function postProcess() {
-    $values = $this->exportValues();
-    // Write to settings.
-
-    CRM_Core_Session::setStatus(ts('Log Lopping settings updated'));
+  function postProcess() {
+    $this->_submittedValues = $this->exportValues();
+    $this->saveSettings();
     parent::postProcess();
   }
+
+  /**
+   * Get the fields/elements defined in this form.
+   *
+   * @return array (string)
+   */
+  function getRenderableElementNames() {
+    // The _elements list includes some items which should not be
+    // auto-rendered in the loop -- such as "qfKey" and "buttons". These
+    // items don't have labels. We'll identify renderable by filtering on
+    // the 'label'.
+    $elementNames = array();
+    foreach ($this->_elements as $element) {
+      $label = $element->getLabel();
+      if (!empty($label)) {
+        $elementNames[] = $element->getName();
+      }
+    }
+    return $elementNames;
+  }
+
+  /**
+   * Get the settings we are going to allow to be set on this form.
+   *
+   * @return array
+   */
+  function getFormSettings() {
+    if (empty($this->_settings)) {
+      $settings = civicrm_api3('setting', 'getfields', array('filters' => $this->_settingFilter));
+    }
+    $settings = $settings['values'];
+    return $settings;
+  }
+
+  /**
+   * Get the settings we are going to allow to be set on this form.
+   *
+   * @return array
+   */
+  function saveSettings() {
+    $settings = $this->getFormSettings();
+    $values = array_intersect_key($this->_submittedValues, $settings);
+    civicrm_api3('setting', 'create', $values);
+  }
+
+  /**
+   * Set defaults for form.
+   *
+   * @see CRM_Core_Form::setDefaultValues()
+   */
+  function setDefaultValues() {
+    $existing = civicrm_api3('setting', 'get', array('return' => array_keys($this->getFormSettings())));
+    $defaults = array();
+    $domainID = CRM_Core_Config::domainID();
+    foreach ($existing['values'][$domainID] as $name => $value) {
+      $defaults[$name] = $value;
+    }
+    return $defaults;
+  }
 }
+
